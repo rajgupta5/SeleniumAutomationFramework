b3f8f2de4dcfe589bc4572ddbd8f17154e52a304
Raj Gupta <rajkumargupta1@gmail.com>
2021-03-27 23:45:46 +0530

    webdriver manager

--- src/main/java/com/selenium/driver/Driver.java
+++ src/main/java/com/selenium/driver/Driver.java
@@ -3,6 +3,7 @@ package com.selenium.driver;
 import com.selenium.constants.FrameworkConstants;
 import com.selenium.enums.ConfigProperties;
 import com.selenium.utils.PropertyUtil;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -17,11 +18,13 @@ public final class Driver {
     public static void initDriver(String browser) throws Exception {
         if(Objects.isNull(DriverManager.getDriver())) {
             if(browser.equalsIgnoreCase("chrome")) {
-                System.setProperty("webdriver.chrome.driver", FrameworkConstants.getCHROMEDRIVERPATH());
+//                System.setProperty("webdriver.chrome.driver", FrameworkConstants.getCHROMEDRIVERPATH());
+                WebDriverManager.chromedriver().setup();
                 DriverManager.setDriver(new ChromeDriver());
             }
             else if(browser.equalsIgnoreCase("firefox")) {
-                System.setProperty("webdriver.gecko.driver", FrameworkConstants.getGECKODRIVERPATH());
+//                System.setProperty("webdriver.gecko.driver", FrameworkConstants.getGECKODRIVERPATH());
+                WebDriverManager.firefoxdriver().setup();
                 DriverManager.setDriver(new FirefoxDriver());
             }
             DriverManager.getDriver().get(PropertyUtil.get(ConfigProperties.URL));
